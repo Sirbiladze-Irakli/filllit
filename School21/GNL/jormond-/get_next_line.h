44be4bbcd4013cdc15f4a873d691fcaa1d94232a
Jason mallister Ormond yronwood <jormond-@at-d4.21-school.ru>
2019-06-14 19:17:00 +0300

    GNL

---
+++ School21/GNL/jormond-/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jormond- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/21 12:21:02 by jormond-          #+#    #+#             */
+/*   Updated: 2019/06/06 13:26:28 by jormond-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <sys/fcntl.h>
+
+# define BUFF_SIZE	5
+
+int 	get_next_line(const int fd, char **line);
+
+#endif
